bec18a780dba0ff3acb24af95520c575bb35f27b
Utkarsh <utkarsh.yadav5@gmail.com>
2017-05-23 22:17:09 +0530

    Accuracy 0.994

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,12 +20,17 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
 from sklearn import svm
-clf=svm.SVC(C=1.0, kernel='linear', gamma='auto')
+clf=svm.SVC(C=10000.0, kernel='linear', gamma='auto')
+# using kernel=rbf && C=10000 => accuracy=0.990
+# using kernel=linear && C=1.0 => accuracy= 0.98
+# using kernel=linear && C=10000 => accuracy= 0.994
 
 t0=time()
 clf.fit(features_train,labels_train)
@@ -35,6 +40,15 @@ t0=time()
 pred=clf.predict(features_test)
 print "prediciting time :", round(time()-t0,3), "s"
 print(pred)
+
+# for counting the number of chris=1
+count=0
+for i in pred:
+    if i==1:
+        count+=1
+
+print count
+#####################
 
 from sklearn.metrics import accuracy_score
 print(accuracy_score(pred,labels_test))
