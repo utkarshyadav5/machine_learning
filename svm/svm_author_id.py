d888a1d9cd4ad51f17b24e7e6c48dffd681d1edb
Utkarsh <utkarsh.yadav5@gmail.com>
2017-05-23 21:23:35 +0530

    SVM for the same mini-project(email) used in naive bayes with accuracy 0.98

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf=svm.SVC(C=1.0, kernel='linear', gamma='auto')
 
+t0=time()
+clf.fit(features_train,labels_train)
+print "training time :", round(time()-t0,3), "s"
+
+t0=time()
+pred=clf.predict(features_test)
+print "prediciting time :", round(time()-t0,3), "s"
+print(pred)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(pred,labels_test))
 #########################################################
 
 
