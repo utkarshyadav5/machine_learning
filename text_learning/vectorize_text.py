a0fe90ae788a3d4c8e3afbc84c46d87740d8637e
Utkarsh <utkarsh.yadav5@gmail.com>
2017-05-26 20:04:49 +0530

    Text learning completed with stemmer and tfidf

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,21 +41,25 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            words=parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            words=words.replace('sara','')
+            words=words.replace('shackleton','')
+            words=words.replace('chris','')
+            words=words.replace('germani','')
             ### append the text to word_data
-
+            word_data.append(words)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name=="sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -71,5 +75,10 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer=TfidfVectorizer(stop_words="english")
+vectorizer.fit(word_data)
+print len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[34597]
 
