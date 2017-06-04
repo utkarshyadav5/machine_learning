df86a342c0333e8aa4d2d0155e1da399dff3bb28
Utkarsh <utkarsh.yadav5@gmail.com>
2017-06-05 01:14:10 +0530

    Lesson 13 completed

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -21,12 +21,21 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
-
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
 
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "Accuracy score:", clf.score(features_test,labels_test)
 
